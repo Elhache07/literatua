d8b25ab913099300837742d4630f1136de85f0d5
elhache07 <manuelhache723@gmail.com>
2025-06-15 10:24:15 -0500

    Agregando proyecto

---
+++ src/main/java/com/java/literatua/LiteratuaApplication.java
@@ -0,0 +1,13 @@
+package com.java.literatua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteratuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteratuaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/literatua/LiteratuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.literatua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteratuaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
